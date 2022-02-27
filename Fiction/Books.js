36841957776e9bf0221bd38cd4a0a76c65a3254b
saloni-1011 <saloni1940.cse19@chitkara.edu.in>
2022-02-27 11:25:49 +0530

    new assignment

---
+++ Fiction/Books.js
@@ -0,0 +1,6 @@
+function func1(){
+    alert('Your response was submitted.')
+}
+function toggle(){
+	document.getElementById("navbar").classList.toggle('active');
+}
---
+++ Horror/srct.js
@@ -0,0 +1,6 @@
+function f() {
+window.alert("Thanks for submitting your response");
+}
+function f1() {
+window.alert("You are being directed to another page");
+}
